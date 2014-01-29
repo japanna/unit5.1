6f3caa6a675d1cd6a3646d7c1cffa702754c28ca
Anna McKelvey <anna.e.mckelvey@gmail.com>
2014-01-29 16:14:45 -0600

    Added comments for  5.1.6

--- LowestGrade.java
+++ LowestGrade.java
@@ -2,70 +2,78 @@
  *  unit 5.1.6
  *  
  *  @author:  Anna Ntenta
- *  @version: Last Modified January 29, 2014
+ *  @version: Last Modified January ___________, 2014
  */
+
+
+import java.util.*;
+
+class LowestGrade
+{
 
 /** 
  *  removeLowest accepts a variable number of integer arguments 
  *  that represent the homework scores of a single student and
  *  returns an array with all the scores except the lowest one.
  *
- *  @param   arrayIn    An array of integers
- *  @return  arrayOut   An array that contains all of the values 
- *						passed to the method EXCEPT for the lowest score.
+ *  @param   args    	 a variable number of integers
+ *  @return  highScores  An array that contains all of the values 
+ *						 passed to the method EXCEPT for the lowest score.
  */
-import java.util.*;
-
-class LowestGrade
-{
-
 private static int [] removeLowest (int... args)
 {
-	if (args.length != 0)
+	// if the method is given no arguments
+	if (args.length == 0)
 	{
-	
-	int [] arr = new int [args.length];
-	int arrIndex = 0;
-    for (int arg : args)
-    {
-    	arr[arrIndex] = arg;
-    	arrIndex++;
-    }
+		// return an empty array
+		int [] highScores = new int [0];
+		return highScores;
+	}
 
-    if (args.length == 1)
+	else 
+
+	{
+		// initiate an array with the length equal to no of arguments
+		int [] arr = new int [args.length];
+		// counter for array
+		int arrIndex = 0;
+		// put each argument in the array
+	    for (int arg : args)
+	    {
+	    	arr[arrIndex] = arg;
+	    	arrIndex++;
+	    }
+	    // if given only one argument the one score is kept
+	    if (args.length == 1)
 		{
 			return arr;
 		}
 
-	// this int will hold the index of the lowest score
-	int lowestIndex = 0;
-	// this array will contain all scores except the lowest
-	// it will be one shorter than the original array
-	int [] highScores = new int [arr.length - 1];
-	// find the index of the lowest score
-	for (int i = 0; i < arr.length; i++)
-	{
-		if (arr[i] < arr[lowestIndex])
-			lowestIndex = i;
-	}
-	// counter for placement of scores in new array
-	int newArrCounter = 0; 
-	// put all scores except lowest in new array
-	for (int j = 0; j < arr.length; j++)
-	{
-		if (j != lowestIndex)
+		// int to hold the index of the lowest score
+		int lowestIndex = 0;
+		// array to contain all scores except the lowest
+		// it will be one shorter than the original array
+		int [] highScores = new int [arr.length - 1];
+		// find the index of the lowest score
+		for (int i = 0; i < arr.length; i++)
 		{
-			highScores[newArrCounter] = arr[j];
-			newArrCounter++;
+			if (arr[i] < arr[lowestIndex])
+				lowestIndex = i;
 		}
+		// counter for placement of scores in new array
+		int newArrCounter = 0; 
+		// put all scores except lowest in new array
+		for (int j = 0; j < arr.length; j++)
+		{
+			if (j != lowestIndex)
+			{
+				highScores[newArrCounter] = arr[j];
+				newArrCounter++;
+			}
+		}
+		// return array of scores except the lowest
+		return highScores;
 	}
-	return highScores;
-}
-else
-{
-	int [] highScores = new int [0];
-	return highScores;
-}
 }
 
 /** 
@@ -83,7 +91,7 @@ else
 public static void main (String [] args)
 {
 	int [] a = removeLowest ( 23, 90, 47, 55, 88);
- 	int [] b = removeLowest ( 85, 1 );
+ 	int [] b = removeLowest ( 85);
  	int [] c = removeLowest ();
  	int [] d = removeLowest (59, 92, 93, 47, 88, 47);
 
