36409513ef2096c5ebea70261cc675ce8a6c00e1
Anna McKelvey <anna.e.mckelvey@gmail.com>
2014-02-14 10:12:39 -0600

    Final versions

--- LowestGrade.java
+++ LowestGrade.java
@@ -21,86 +21,106 @@ class LowestGrade
  *						 passed to the method EXCEPT for the lowest score.
  */
 private static int [] removeLowest (int... args)
-{
-	// if the method is given no arguments
-	if (args.length == 0)
 	{
-		// return an empty array
-		int [] highScores = new int [0];
-		return highScores;
-	}
-
-	else 
-
-	{
-		// initiate an array with the length equal to no of arguments
-		int [] arr = new int [args.length];
-		// counter for array
-		int arrIndex = 0;
-		// put each argument in the array
-	    for (int arg : args)
-	    {
-	    	arr[arrIndex] = arg;
-	    	arrIndex++;
-	    }
-	    // if given only one argument the one score is kept
-	    if (args.length == 1)
+		// if the method is given no arguments
+		if (args.length == 0)
 		{
-			return arr;
+			// return an empty array
+			int [] highScores = new int [0];
+			return highScores;
 		}
 
-		// int to hold the index of the lowest score
-		int lowestIndex = 0;
-		// array to contain all scores except the lowest
-		// it will be one shorter than the original array
-		int [] highScores = new int [arr.length - 1];
-		// find the index of the lowest score
-		for (int i = 0; i < arr.length; i++)
+		else 
+
 		{
-			if (arr[i] < arr[lowestIndex])
-				lowestIndex = i;
-		}
-		// counter for placement of scores in new array
-		int newArrCounter = 0; 
-		// put all scores except lowest in new array
-		for (int j = 0; j < arr.length; j++)
-		{
-			if (j != lowestIndex)
+			// initiate an array with the length equal to no of arguments
+			int [] arr = new int [args.length];
+			// counter for array
+			int arrIndex = 0;
+			// put each argument in the array
+		    for (int arg : args)
+		    {
+		    	arr[arrIndex] = arg;
+		    	arrIndex++;
+		    }
+		    // if given only one argument the one score is kept
+		    if (args.length == 1)
 			{
-				highScores[newArrCounter] = arr[j];
-				newArrCounter++;
+				return arr;
 			}
+
+			// int to hold the index of the lowest score
+			int lowestIndex = 0;
+			// array to contain all scores except the lowest
+			// it will be one shorter than the original array
+			int [] highScores = new int [arr.length - 1];
+			// find the index of the lowest score
+			for (int i = 0; i < arr.length; i++)
+			{
+				if (arr[i] < arr[lowestIndex])
+					lowestIndex = i;
+			}
+			// counter for placement of scores in new array
+			int newArrCounter = 0; 
+			// put all scores except lowest in new array
+			for (int j = 0; j < arr.length; j++)
+			{
+				if (j != lowestIndex)
+				{
+					highScores[newArrCounter] = arr[j];
+					newArrCounter++;
+				}
+			}
+			// return array of scores except the lowest
+			return highScores;
 		}
-		// return array of scores except the lowest
-		return highScores;
 	}
-}
 
 /** 
  *  arrayPrint accepts an array of integers and prints it out as a string 
  *
  *  @param   arrayIn    An array of integers
  *  @return  String   	A string containing the same integers
-*
-*private static String arrayPrint (int [] arr)
-*{
-*
-*}
- */
+*/
+private static String arrayPrint (int [] arr)
+	{
+		// get length of array once
+		int len = arr.length;
+
+		// if array is empty return empty brackets
+		if (len == 0) return "[]";
+
+		// create a StringBuilder object
+		StringBuilder newString = new StringBuilder();
+		// append left bracket to the new stringbuilder
+		newString.append("[");
+		// append all but the last element of the array 
+		for(int i = 0; i < len - 1; i++)
+		{
+			// space and commas between each element
+			newString.append( arr[i] + ", ");
+		}
+		// append the last element of the array to the new string
+		// together with a right bracket
+		newString.append(arr[len-1] + "]");
+
+		// convert stringbuilder to string
+		return newString.toString();
+	}
+ 
 
 public static void main (String [] args)
-{
-	int [] a = removeLowest ( 23, 90, 47, 55, 88);
- 	int [] b = removeLowest ( 85);
- 	int [] c = removeLowest ();
- 	int [] d = removeLowest (59, 92, 93, 47, 88, 47);
+	{
+		int [] a = removeLowest ( 23, 90, 47, 55, 88);
+	 	int [] b = removeLowest ( 85);
+	 	int [] c = removeLowest ();
+	 	int [] d = removeLowest (59, 92, 93, 47, 88, 47);
 
- 	System.out.println("a = " + Arrays.toString(a));
- 	System.out.println("a = " + Arrays.toString(b));
- 	System.out.println("a = " + Arrays.toString(c));
- 	System.out.println("a = " + Arrays.toString(d));
- 	//System.out.println("b = " + arrayPrint(b));
- 	//System.out.println("c = " + arrayPrint(c));
- 	//System.out.println("d = " + arrayPrint(d));
-}
+	 	System.out.println();
+	 	System.out.println("a = " + arrayPrint(a));
+	 	System.out.println("b = " + arrayPrint(b));
+	 	System.out.println("c = " + arrayPrint(c));
+	 	System.out.println("d = " + arrayPrint(d));
+	 	System.out.println();
+	}
 }
---
+++ Palindrome.java
@@ -0,0 +1,76 @@
+/** Palindrome.java
+ *  unit 5.1.7
+ *  
+ *  @author:  Anna Ntenta
+ *  @version: Last Modified February 14, 2014
+ */
+
+
+import java.util.*;
+
+class Palindrome
+{
+	public static boolean isPalindrome (String str)
+	{
+		// get the length of the original string once
+		int original_len = str.length();
+		// convert all chars to uppercase
+		String upper = str.toUpperCase();
+		// create new string builder
+		StringBuilder newString = new StringBuilder();
+
+		// build new string that consists of only letters
+		for(int i = 0; i < original_len; i++)
+		{
+			if (Character.isLetter(upper.charAt(i))) {
+			newString.append(upper.charAt(i) );
+			}
+		}
+		// convert string builder to string
+		String s = newString.toString();
+		// get length of new string
+		int len = s.length();
+
+		// base case
+		if (len <= 1) {
+			return true;
+		}
+		// recursive case
+		// if first and last char are the same
+		if (s.charAt(0) == s.charAt(len - 1)) 
+		{
+			// create a substring
+			String sub = s.substring(1, len - 1);
+			// check if the substring is a palindrome
+			return isPalindrome(sub);
+		}
+		// if first and last char are *not* the same it's not a palindrome
+		else 
+			return false;
+	}
+	
+
+public static void main (String [] args)
+	{
+		// create Scanner object
+		Scanner in = new Scanner (System.in);
+
+		System.out.print ("Please write a sentence: ");
+
+		// make sure that user provided correct input
+		if(in.hasNextLine()) {
+
+			String input = in.nextLine();
+
+			if (isPalindrome(input)) {
+				System.out.println("\n Yay! \"" + input + "\" is a palindrome!\n");
+			}
+			else {
+				System.out.println("\nNay! \"" + input + "\" is NOT a palindrome!\n");
+			}
+		}
+		// error message if user didn't provide a string
+		else System.out.println("\n\nI'm sorry Dave. I'm afraid I can't let you do that.\nPlease write something that can be of type String.\n");
+	}
+}
+
